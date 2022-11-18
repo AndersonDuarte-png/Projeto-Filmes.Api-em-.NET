adc99fcc5e8dc1c8434ecd8224bb8f06726e1a88
AndersonDuarte-png <anderson.duarte889@gmail.com>
2022-11-18 11:16:12 -0300

    add trabalho testado

---
+++ Netflix.Api/Controllers/FilmeApi.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Netflix.Api.Models;
+using Netflix.Api.Services;
+
+namespace Netflix.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeApi : ControllerBase
+{
+    private readonly IFilmeService _filmeService;
+
+    public FilmeApi(IFilmeService filmeService)
+    {
+        _filmeService = filmeService;
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> Get(){
+        return _filmeService.ListarFilmes();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(string id){
+        var filme = _filmeService.ObterFilme(id);
+        if(filme!= null){
+            
+            return Ok(filme);
+        }
+        else{
+            return BadRequest("Não encontrado");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Create(Filme filme)
+    {
+        if (_filmeService.SalvarFilme(filme)){
+            return Ok();
+        }
+        else{
+            return BadRequest("");
+        }
+    }
+
+    [HttpPut]
+    public IActionResult Update(Filme filme)
+    {
+        if (_filmeService.AlterarFilme(filme)){
+            return Ok();
+        }
+        else{
+            return BadRequest("");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete( string id){
+        if (_filmeService.DeletarFilme(id)){
+            return Ok();
+        }
+        else{
+            return BadRequest("");
+        }
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Projeto-Filmes.Api-em-.NET
 Trabalho [API] - Projeto Filmes.Api em .NET
+
+dotnet run --project Netflix.Api
